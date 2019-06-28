bec532acd2333c69ae6dc304f30aa45907844826
aspenjames <aspenjames@tqca.org>
2019-06-28 12:01:27 -0700

    lecture end

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,6 +1,10 @@
 class AnimalsController < ApplicationController
 	def index
 		@animals = Animal.all
+		respond_to do |format|
+			format.html { render :index }
+			format.json { render :json => @animals, include: :species }
+		end
   end
 
 	def show
@@ -13,9 +17,15 @@ class AnimalsController < ApplicationController
   end
 
 	def create
-		byebug
+		@animal = Animal.create(animal_params)
+		redirect_to animals_path
 	end
 	
 	def home
+	end
+
+	private
+	def animal_params
+		params.require(:animal).permit(:name, :age, :species_id)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	resources :animals, :only => %i(index new create show)
 	root :to => 'animals#home'
+
+	namespace 'api' do
+		get '/animals', to: 'api#animals'
+	end
 end
